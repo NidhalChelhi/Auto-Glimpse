d3889f8a96a06eb374fbcadd3d93e4f5452f88a7
NidhalChelhi <nidhalelchelhi@gmail.com>
2023-08-24 20:23:48 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import { Footer, Navbar } from "@/components";
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "AutoGlimpse",
+  description: "Discover the best cars in the world.",
+
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <link rel="icon" href="/favicon.png" sizes="any" />
+
+      <body className="relative">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
