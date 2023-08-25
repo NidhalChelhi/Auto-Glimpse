6a4d5089f74d78f2024fe530c063acadfaa23900
NidhalChelhi <nidhalelchelhi@gmail.com>
2023-08-25 01:35:22 +0100

    Fixing Footer Copyrights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "AutoGlimpse",
+  title: "Auto Glimpse",
   description: "Discover the best cars in the world.",
 
 };
